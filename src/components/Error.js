1c71465fc6bba6b55370e85ebfb4729a6aa7f001
amrinaas <amrinaas96@gmail.com>
2023-08-15 21:40:07 +0700

    Add components

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className="section section-center text-center">
+      <h2>There was en error...</h2>
+    </div>
+  )
+}
+
+export default Error
